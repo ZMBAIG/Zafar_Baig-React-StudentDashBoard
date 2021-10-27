60ffa70faefee5d985a7d161e920723b92c73422
Zafar Baig <49963556+ZMBAIG@users.noreply.github.com>
2021-10-27 13:11:05 +0200

    Add files via upload

---
+++ src/components/WincFooter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function wincFooter() {
+  return (
+    <footer>
+      <span>WINC ACADEMY © </span>
+      <a href="mailto:wincacademy@example.com">wincacademy@example.com</a>
+    </footer>
+  );
+}
+
+export default wincFooter;
